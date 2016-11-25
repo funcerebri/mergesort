101f3f2b8e70f8ee005861b5ed8796cc5af23224
funcerebri <valera.pogor@gmail.com>
2016-11-26 00:55:53 +0200

    Implement algorithm of merge sort

--- MergeSort/MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort/MergeSort.cpp
@@ -1,11 +1,111 @@
 // MergeSort.cpp : Defines the entry point for the console application.
 //
-
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <fstream>
+#include <string>
 #include "stdafx.h"
+#include<stdlib.h>
+#include<stdio.h>
+#include <vector>
+using namespace std;
 
-
-int main()
+/* Function to print an array */
+void printArray(int A[], int size)
 {
-    return 0;
+	int i;
+	for (i = 0; i < size; i++)
+		printf("%d ", A[i]);
+	printf("\n");
 }
 
+template<typename T>
+void MergeSortAlg(T a[], size_t l)
+{
+	size_t BlockSizeIterator;
+	size_t BlockIterator;
+	size_t LeftBlockIterator;
+	size_t RightBlockIterator;
+	size_t MergeIterator;
+
+	size_t LeftBorder;
+	size_t MidBorder;
+	size_t RightBorder;
+	for (BlockSizeIterator = 1; BlockSizeIterator < l; BlockSizeIterator *= 2)
+	{
+		for (BlockIterator = 0; BlockIterator < l - BlockSizeIterator; BlockIterator += 2 * BlockSizeIterator)
+		{
+			//ѕроизводим сли€ние с сортировкой пары блоков начинающуюс€ с элемента BlockIterator
+			//левый размером BlockSizeIterator, правый размером BlockSizeIterator или меньше
+			LeftBlockIterator = 0;
+			RightBlockIterator = 0;
+			LeftBorder = BlockIterator;
+			MidBorder = BlockIterator + BlockSizeIterator;
+			RightBorder = BlockIterator + 2 * BlockSizeIterator;
+			RightBorder = (RightBorder < l) ? RightBorder : l;
+			int* SortedBlock = new int[RightBorder - LeftBorder];
+
+			//ѕока в обоих массивах есть элементы выбираем меньший из них и заносим в отсортированный блок
+			while (LeftBorder + LeftBlockIterator < MidBorder && MidBorder + RightBlockIterator < RightBorder)
+			{
+				if (a[LeftBorder + LeftBlockIterator] < a[MidBorder + RightBlockIterator])
+				{
+					SortedBlock[LeftBlockIterator + RightBlockIterator] = a[LeftBorder + LeftBlockIterator];
+					LeftBlockIterator += 1;
+				}
+				else
+				{
+					SortedBlock[LeftBlockIterator + RightBlockIterator] = a[MidBorder + RightBlockIterator];
+					RightBlockIterator += 1;
+				}
+			}
+			//ѕосле этого заносим оставшиес€ элементы из левого или правого блока
+			while (LeftBorder + LeftBlockIterator < MidBorder)
+			{
+				SortedBlock[LeftBlockIterator + RightBlockIterator] = a[LeftBorder + LeftBlockIterator];
+				LeftBlockIterator += 1;
+			}
+			while (MidBorder + RightBlockIterator < RightBorder)
+			{
+				SortedBlock[LeftBlockIterator + RightBlockIterator] = a[MidBorder + RightBlockIterator];
+				RightBlockIterator += 1;
+			}
+
+			for (MergeIterator = 0; MergeIterator < LeftBlockIterator + RightBlockIterator; MergeIterator++)
+			{
+				a[LeftBorder + MergeIterator] = SortedBlock[MergeIterator];
+			}
+			delete SortedBlock;
+		}
+	}
+}
+
+#define N 80
+int main()
+{
+	FILE *file;
+	FILE *fileout;
+	int arr[N];
+
+	file = fopen("input.txt", "r");
+	fileout = fopen("output.txt", "w+");
+
+	for (int i = 0; i < 80; i++) {
+		fscanf(file, "%d", &arr[i]);
+	}
+	fclose(file);
+
+	int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+	printf("Given array is \n");
+	printArray(arr, arr_size);
+	MergeSortAlg(arr, arr_size);
+	printf("\nSorted array is \n");
+	printArray(arr, arr_size);
+
+	for (int i = 0; i < 80; i++) {
+		fprintf(fileout, "%d%s", arr[i], " ");
+	}
+	fclose(fileout);
+}
