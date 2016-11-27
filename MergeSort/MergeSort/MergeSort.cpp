62454717719ed405380667ae9c8fc57350316776
funcerebri <valera.pogor@gmail.com>
2016-11-28 00:30:08 +0200

    Start parallel part

--- MergeSort/MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort/MergeSort.cpp
@@ -7,7 +7,6 @@
 #include <string>
 #include "stdafx.h"
 #include<stdlib.h>
-
 #include <vector>
 #include<stdio.h>
 #include <time.h>
@@ -84,24 +83,89 @@ void MergeSortAlg(T a[], size_t l)
 	}
 }
 
+
+template<typename T>
+void MergeSortAlgParallel(T a[], size_t l)
+{
+	size_t BlockSizeIterator;
+	size_t BlockIterator;
+	size_t LeftBlockIterator;
+	size_t RightBlockIterator;
+	size_t MergeIterator;
+
+	size_t LeftBorder;
+	size_t MidBorder;
+	size_t RightBorder;
+	for (BlockSizeIterator = 1; BlockSizeIterator < l; BlockSizeIterator *= 2)
+	{
+		for (BlockIterator = 0; BlockIterator < l - BlockSizeIterator; BlockIterator += 2 * BlockSizeIterator)
+		{
+			//ѕроизводим сли€ние с сортировкой пары блоков начинающуюс€ с элемента BlockIterator
+			//левый размером BlockSizeIterator, правый размером BlockSizeIterator или меньше
+			LeftBlockIterator = 0;
+			RightBlockIterator = 0;
+			LeftBorder = BlockIterator;
+			MidBorder = BlockIterator + BlockSizeIterator;
+			RightBorder = BlockIterator + 2 * BlockSizeIterator;
+			RightBorder = (RightBorder < l) ? RightBorder : l;
+			int* SortedBlock = new int[RightBorder - LeftBorder];
+
+			//ѕока в обоих массивах есть элементы выбираем меньший из них и заносим в отсортированный блок
+			while (LeftBorder + LeftBlockIterator < MidBorder && MidBorder + RightBlockIterator < RightBorder)
+			{
+				if (a[LeftBorder + LeftBlockIterator] < a[MidBorder + RightBlockIterator])
+				{
+					SortedBlock[LeftBlockIterator + RightBlockIterator] = a[LeftBorder + LeftBlockIterator];
+					LeftBlockIterator += 1;
+				}
+				else
+				{
+					SortedBlock[LeftBlockIterator + RightBlockIterator] = a[MidBorder + RightBlockIterator];
+					RightBlockIterator += 1;
+				}
+			}
+			//ѕосле этого заносим оставшиес€ элементы из левого или правого блока
+			while (LeftBorder + LeftBlockIterator < MidBorder)
+			{
+				SortedBlock[LeftBlockIterator + RightBlockIterator] = a[LeftBorder + LeftBlockIterator];
+				LeftBlockIterator += 1;
+			}
+			while (MidBorder + RightBlockIterator < RightBorder)
+			{
+				SortedBlock[LeftBlockIterator + RightBlockIterator] = a[MidBorder + RightBlockIterator];
+				RightBlockIterator += 1;
+			}
+
+			for (MergeIterator = 0; MergeIterator < LeftBlockIterator + RightBlockIterator; MergeIterator++)
+			{
+				a[LeftBorder + MergeIterator] = SortedBlock[MergeIterator];
+			}
+			delete SortedBlock;
+		}
+	}
+}
+
 #define N 20000
 int main()
 {
 	int arr[N];
 	
+	//read digits from file and write its into array
 	FILE *file;
-	file = fopen("dsd.txt", "r");
+	file = fopen("input.txt", "r");
 	for (int i = 0; i < N; i++) {
 		fscanf(file, "%d", &arr[i]);
 	}
 	fclose(file);
 
+	//sort array and calculate time
 	clock_t time = clock();
 	int arr_size = sizeof(arr) / sizeof(arr[0]);
 	MergeSortAlg(arr, arr_size);
 	time = clock() - time;
 	double ms = double(time) / CLOCKS_PER_SEC * 1000;
 
+	//write sort array into file
 	FILE *fileout;
 	fileout = fopen("output.txt", "w+");
 	printf("Execution time = %f\n", ms);	
@@ -112,3 +176,24 @@ int main()
 
 	return getchar();
 }
+
+/*#include <boost/thread.hpp>
+
+void task1() { 
+    // do stuff
+}
+
+void task2() { 
+    // do stuff
+}
+
+int main (int argc, char ** argv) {
+    using namespace boost; 
+    thread thread_1 = thread(task1);
+    thread thread_2 = thread(task2);
+
+    // do other stuff
+    thread_2.join();
+    thread_1.join();
+    return 0;
+	}*/
